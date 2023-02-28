0e62a0645d5b8f9ebb2e1a58e6d40eece7993143
Prabhu Karki <112304490+Prabhu-Karki@users.noreply.github.com>
2023-02-28 12:07:18 +0545

    change in Email Backend

--- VacationNepal/settings.py
+++ VacationNepal/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_USER_TLS=True
+EMAIL_USE_TLS=True
 EMAIL_HOST_USER='vacationnepal640@gmail.com'
 EMAIL_HOST_PASSWORD='wkdajcblhltljbiz'
 
